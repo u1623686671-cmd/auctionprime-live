fd7bd2032a4f64224c6b77ee98deab2a7c7ca8fc
Charbel Chawich <u1623686671@gmail.com>
2026-02-24 18:32:22 +0000

    PE_API_KEY ✔ Enter a value for STRIPE_API_KEY ✔  Created new secret vers

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -150,7 +150,7 @@ export function Header() {
           {/* Mobile Logo */}
           <div className="flex items-center md:hidden">
                <Link href="/home" className="flex items-center space-x-2">
-                  <span className="font-extrabold tracking-tight font-headline text-2xl text-primary">Ubid</span>
+                  <span className="font-extrabold tracking-tight font-headline text-2xl text-primary">AuctionPrime</span>
                </Link>
           </div>
           
@@ -240,5 +240,4 @@ export function Header() {
     </>
   );
 }
-
     
